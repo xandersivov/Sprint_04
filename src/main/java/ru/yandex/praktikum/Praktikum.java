08dff4dbfd6a4f6e2029cf1988fe81b317535640
Alexander Sivov <Xandersivov@yandex.ru>
2022-04-24 21:17:36 +0700

    relocate files

---
+++ src/main/java/ru/yandex/praktikum/Praktikum.java
@@ -0,0 +1,11 @@
+package ru.yandex.praktikum;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        String name = "Alexander Sivov";
+        Account account = new Account(name);
+        account.checkNameToEmboss();
+    }
+
+}
