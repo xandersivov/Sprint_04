5ca4b1a7ff3f1848dba4b1a4a3109033528cec3e
Alexander Sivov <Xandersivov@yandex.ru>
2022-04-24 22:03:35 +0700

    устранил замечания по ревью

--- src/main/java/ru/yandex/praktikum/Account.java
+++ src/main/java/ru/yandex/praktikum/Account.java
@@ -9,16 +9,16 @@ public class Account {
     }
 
     public boolean checkNameToEmboss() {
-            if (name == null || name.isEmpty())
-                return false;
-            if (name.length() < 2)
-                return false;
-            if (name.length() > 19)
-                return false;
-            if (name.startsWith(" ") || name.endsWith(" "))
-                return false;
-            if (name.split(" ").length != 2)
-                return false;
+        if (name == null || name.isEmpty())
+            return false;
+        if (name.length() < 2)
+            return false;
+        if (name.length() > 19)
+            return false;
+        if (name.startsWith(" ") || name.endsWith(" "))
+            return false;
+        if (name.split(" ").length != 2)
+            return false;
         return true;
     }
 }
--- src/test/java/ru/yandex/praktikum/AccountTest.java
+++ src/test/java/ru/yandex/praktikum/AccountTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class AccountTest {
@@ -12,30 +12,33 @@ public class AccountTest {
     private final String name;
     private final boolean expected;
 
-    public AccountTest(String name, boolean expected){
+    public AccountTest(String name, boolean expected) {
         this.name = name;
         this.expected = expected;
     }
-    @Parameterized.Parameters
+
+    @Parameterized.Parameters (name = "Test value: {0} - {1}")
     public static Object[][] getTestData() {
-        return new Object[][] {
+        return new Object[][]{
                 {"Тимоти Шаламе", true},  //валидный результат ру
                 {"Timoti Shalome", true}, //валидный результат en
-                {"Т м",true}, // 3 знака с пробелом
-                {"Тим",false}, // 3 знака без пробела
+                {"Т м", true}, // 3 знака с пробелом
+                {"Тим", false}, // 3 знака без пробела
                 {"Тимоти Шаламеееееее", true}, //19 знаков
                 {"Ти", false}, // 2 знака
                 {"Тимотит Шаламеееееее", false}, // 20 знаков
                 {" ТимотиШаламе", false},  //пробел в начале
                 {"ШаламеПробел ", false}, //пробел в конце
                 {"Ти моти Шаламе", false}, //2 пробела
-                {"", false} //пусто
+                {"", false}, //пусто
+                {null, false} //null
         };
     }
+
     @Test
-    public void checkName(){
+    public void checkName() {
         Account account = new Account(name);
         boolean actual = account.checkNameToEmboss();
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
